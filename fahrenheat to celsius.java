e57b01fc75825184c054dece10d3a4f6961586e9
Sakthi Iswarya S <82805914+sakthi-iswarya@users.noreply.github.com>
2023-05-30 15:41:43 +0530

    Add files via upload

---
+++ fahrenheat to celsius.java
@@ -0,0 +1,10 @@
+// fahrenheat to celsius
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        float f = sc.nextFloat();
+        float s = (f-32)*5/9;
+        System.out.println(s);
+    }
+}
